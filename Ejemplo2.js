e38d13e8176f130a048ed6503260d6f4adbde4f1
Wilson Castillo <dc105677543@gmail.com>
2023-02-03 08:27:07 -0500

    first commit

---
+++ Ejemplo2.js
@@ -0,0 +1,7 @@
+let cadena = 'La vida es bella';
+for (let i = 0 ; i < cadena.length ; i++){
+console.log( cadena.substring(i,) );
+}
+
+// con cada vuelta la variable pierde un digito por el bucle for
+//a
---
+++ Strigst.js
@@ -0,0 +1,7 @@
+let cadena = 'Hola comunidad X';
+for (let i = 0 ; i < cadena.length ; i++){
+console.log( cadena.substring(i,) );
+}
+
+// con cada vuelta la variable pierde un digito
+//Salida X
